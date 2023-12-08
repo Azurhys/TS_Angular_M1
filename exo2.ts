9c0e69e8aa155a5909203f036b778fcd2565c29b
Pozymandiaz <lucasverstraete01@gmail.com>
2023-12-08 14:41:50 +0100

    ajout exo2

---
+++ exo2.ts
@@ -0,0 +1,10 @@
+interface Administrateur {
+    nom : string,
+    email : string ,
+    ip : string ,
+    dt_connexion : Date ,
+    login : string,
+    password : string
+   }
+
+   type UtilisateurAnonyme = Omit<Administrateur, 'email' | 'dt_connexion' | 'login' | 'password'> & { nom?: string } & { ip: string };
